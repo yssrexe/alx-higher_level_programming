298cc4b798fc8a17c3d1a00d598e12601e4f5d5c
yassir elyasini <117415601+yssrexe@users.noreply.github.com>
2024-01-16 15:22:09 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if (chr(i) != 'e' and chr(i) != 'q'):
-        print("{}".format(chr(i)), end="")
+for char in range(97, 123):
+    if char != 101 and char != 113:
+        print("{:c}".format(char), end="")
