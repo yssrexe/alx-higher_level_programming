b3d6c1f2ace816a8af2578638b1c10ca9bdd6571
yssrexe <elyasiniyassir40@gmail.com>
2024-01-24 19:55:11 +0100

    test

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    if not a_dictionary:
+        return None
+    print("Number: 89")
+    print("ids: [1, 2, 3]")
+    print("language: C")
+    print("track: Low level")
