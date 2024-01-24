e4d7065faf201d28d6c53e1f7b2bd70fb47b01f6
yssrexe <elyasiniyassir40@gmail.com>
2024-01-24 19:08:11 +0100

    4

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new_list = []
-    for s1 in set_1:
-        for s2 in set_2:
-            if s1 == s2:
-                new_list = s1
-    return new_list
+    return set_1 & set_2
