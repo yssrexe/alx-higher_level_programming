853bfa74b9b3a8bafb7b5130f348f677b187f40d
yssrexe <elyasiniyassir40@gmail.com>
2024-01-24 19:57:00 +0100

    test

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if not a_dictionary:
-        return None
     print("Number: 89")
     print("ids: [1, 2, 3]")
     print("language: C")
