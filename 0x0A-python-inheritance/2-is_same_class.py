55a0bf7c16102343c48fa2a966eb9fdb4933721f
yassir elyasini <elyasiniyassir40@gmail.com>
2024-02-19 11:55:56 +0100

    task 3

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Prototype: def is_same_class(obj, a_class):
+obj - class
+return true or false
+"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if the object is exactly an instance of the class"""
+    if type(obj) == a_class:
+        return True
+    return False
