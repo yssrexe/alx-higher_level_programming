c6812d502bee49d00bb16bed9ff35dee8660da77
yassir elyasini <elyasiniyassir40@gmail.com>
2024-02-19 12:41:22 +0100

    task 7

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+6-base_geometry.py
+class:
+BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
+
+
+def area(self):
+    """def area"""
+    raise Exception("area() is not implemented")
