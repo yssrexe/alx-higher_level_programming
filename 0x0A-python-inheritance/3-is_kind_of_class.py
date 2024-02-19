5e9527f94380547570267fec45801703fe8a4250
yassir elyasini <elyasiniyassir40@gmail.com>
2024-02-19 12:09:16 +0100

    task 4

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+3-is_kind_of_class.py
+Prototype:
+def is_kind_of_class(obj, a_class):
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    return : true or false
+    """
+    return isinstance(obj, a_class)
