71a30478013087f60323248bcd86ecdb31ec62d5
yassir elyasini <elyasiniyassir40@gmail.com>
2024-02-20 19:06:11 +0100

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+3-to_json_string.py
+Prototype:
+def to_json_string(my_obj):
+"""
+
+
+def to_json_string(my_obj):
+    """def to_json_string"""
+    import json
+    return json.dumps(my_obj)
