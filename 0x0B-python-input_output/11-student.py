50ee10903f1ffa5aa93c17e07cccc4c871bfacb2
yassir elyasini <elyasiniyassir40@gmail.com>
2024-02-20 20:36:12 +0100

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This is '11-student' module.
+Functions and Classes:
+    class Student
+"""
+
+
+class Student():
+    """representing a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance
+        """
+        my_dict = {}
+        for item in dir(self):
+            condition_1 = not item.startswith("__")
+            condition_2 = not callable(getattr(self, item))
+            if type(attrs) is list:
+                condition_3 = item in attrs
+            else:
+                condition_3 = True
+            if condition_1 and condition_2 and condition_3:
+                my_dict[item] = getattr(self, item)
+
+        return my_dict
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of a Student instance
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
