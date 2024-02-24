ba14d2cb3b185d1fbb3b18a2a46faf44d45625c9
yassir elyasini <elyasiniyassir40@gmail.com>
2024-02-24 12:22:04 +0100

    task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,7 @@ class Rectangle(Base):
     def display(self):
         for _ in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+    
