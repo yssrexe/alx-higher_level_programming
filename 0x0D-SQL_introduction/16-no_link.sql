7cd2b9c864840fb43e47605bfd2e00b8a7a60662
yassir elyasini <elyasiniyassir40@gmail.com>
2024-03-26 17:46:56 +0000

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,3 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0
-DELETE FROM second_table WHERE name="George";
-INSERT INTO second_table(score, name) VALUES(18, "Aria");
-INSERT INTO second_table(score, name) VALUES(12, "Aria");
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
