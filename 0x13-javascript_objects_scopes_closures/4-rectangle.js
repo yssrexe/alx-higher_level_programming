81c9191866fdee37345d6907c369c8685bd9091b
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-23 23:49:20 +0100

    task 04

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h){
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    print()
+    {
+        for (let index = 0; index < this.height; index++) {
+            console.log('X'.repeat(this.width, 10))
+        }
+    }
+
+    double()
+    {
+        for (let index = 0; index < this.height * 2; index++) {
+            console.log('X'.repeat(this.width * 2, 10))
+        }
+    }
+
+    rotate()
+    {
+        for (let index = 0; index < this.width * 2; index++) {
+            console.log('X'.repeat(this.height * 2, 10))
+        }
+    }
+}
+
+module.exports = Rectangle;
