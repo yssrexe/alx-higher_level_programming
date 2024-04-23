f85a3a79f216910a2bd23563f20c5caaf8d08b43
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-23 23:56:08 +0100

    rask 04

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,29 +1,26 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h){
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print()
-    {
-        for (let index = 0; index < this.height; index++) {
-            console.log('X'.repeat(this.width, 10))
-        }
+  print () {
+    for (let index = 0; index < this.height; index++) {
+      console.log('X'.repeat(this.width, 10));
     }
+  }
 
-    rotate()
-    {
-        [this.width, this.height] = [this.height, this.width]
-    }
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
 
-    double()
-    {
-        this.height *= 2;
-        this.width *= 2;
-    }
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
 }
 
 module.exports = Rectangle;
