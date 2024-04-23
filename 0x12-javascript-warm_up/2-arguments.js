948c35101adbe1a22da7581a331483d2038a1751
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-23 01:33:40 +0100

    task 02

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@ const procces = require('process');
 const args = procces.argv;
 const arglenth = args.length;
 
-if (arglenth === 1) {
+if (arglenth === 2) {
   console.log('No argument');
-} else if (arglenth === 2) {
+} else if (arglenth === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
