42c47fcfd8712f5ddb17892c26a2aef82050fa1b
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-25 20:37:30 +0100

    task 04

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+def ShowAll():
+    args = sys.argv
+    mydb = MySQLdb.connect(
+        host="localhost",
+        port = 3306,
+        user= args[1],
+        password= args[2],
+        database= args[3],
+        charset = "utf8"
+    )
+    mycursor=mydb.cursor()
+    mycursor.execute("""SELECT c.id, c.name, s.name
+                    FROM cities AS c
+                    JOIN states AS s
+                    ON c.state_id = s.id
+                    ORDER BY c.id ASC
+                """)
+    result = mycursor.fetchall()
+    
+    for row in result:
+        print(row)
+        
+        
+if __name__ == "__main__":
+    ShowAll()
+
