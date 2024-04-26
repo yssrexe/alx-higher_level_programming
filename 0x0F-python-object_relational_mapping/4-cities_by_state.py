ab6fa8280165ffbecbb16db755ec614963165916
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-26 12:18:55 +0100

    task 4 betty

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-
+"""print all states from hbtn_0e_0_usa database"""
 import MySQLdb
 import sys
+
 
 def ShowAll():
     args = sys.argv
     mydb = MySQLdb.connect(
         host="localhost",
-        port = 3306,
-        user= args[1],
-        password= args[2],
-        database= args[3],
-        charset = "utf8"
+        port=3306,
+        user=args[1],
+        password=args[2],
+        database=args[3],
+        charset="utf8"
     )
-    mycursor=mydb.cursor()
+
+    mycursor = mydb.cursor()
     mycursor.execute("""SELECT c.id, c.name, s.name
                     FROM cities AS c
                     JOIN states AS s
@@ -21,11 +23,10 @@ def ShowAll():
                     ORDER BY c.id ASC
                 """)
     result = mycursor.fetchall()
-    
+
     for row in result:
         print(row)
-        
-        
+
+
 if __name__ == "__main__":
     ShowAll()
-
