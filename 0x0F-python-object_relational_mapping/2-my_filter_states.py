ff08587f921a5bbe884333081f64c1c7c4c235e3
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-26 12:07:37 +0100

    betty

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-
+"""print all states from hbtn_0e_0_usa database"""
 import MySQLdb
 import sys
+
 
 def FilSt():
     args = sys.argv
     mydb = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user = args[1],
-        password = args[2],
-        database= args[3],
+        user=args[1],
+        password=args[2],
+        database=args[3],
         charset="utf8"
     )
-    mycursos= mydb.cursor()
-    mycursos.execute("""SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC""".format(args[4]))
+
+    mycursos = mydb.cursor()
+    mycursos.execute("""SELECT * FROM states WHERE BINARY name = '{}'
+                     ORDER BY id ASC""".format(args[4]))
 
     result = mycursos.fetchall()
+
     for raw in result:
         print(raw)
-        
+
+
 if __name__ == "__main__":
     FilSt()
