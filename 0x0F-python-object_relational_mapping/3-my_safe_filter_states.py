47622e5cec89c3660bea4698b37aeca2e4161483
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-26 12:09:23 +0100

    betty

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,20 +4,22 @@ import sys
 """
    a script that takes in an argument and displays all values
 """
+
+
 if __name__ == "__main__":
     args = sys.argv
     mydb = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user = args[1],
-        password = args[2],
-        database= args[3],
+        user=args[1],
+        password=args[2],
+        database=args[3],
         charset="utf8"
     )
+
     mycursor = mydb.cursor()
     stat = """SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"""
-    mycursor.execute(stat ,(args[4],))
+    mycursor.execute(stat, (args[4],))
     result = mycursor.fetchall()
     for row in result:
         print(row)
-
