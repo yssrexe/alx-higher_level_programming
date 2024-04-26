f23f6fba8349ab63b0528b709697ddac5ca12f04
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-26 22:54:14 +0100

    task 7 betty

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import select, create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    args = sys.argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        args[1], args[2], args[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(state.id, state.name, sep=": ")
