059e9061f44748c32e52b69504ebd1d1bf03fd75
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-26 12:16:20 +0100

    task 3 bety

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 """
-   a script that takes in an argument and displays all values
+This script displays the 'states' table in a Safe way
 """
 
 
 def main():
-    args = sys.argv
-    mydb = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=args[1],
-        password=args[2],
-        database=args[3],
-        charset="utf8"
-    )
+    """
+    displays all values in the states table of hbtn_0e_0_usa
+    where name matches the argument, but safe from SQL Injections
+    """
+    import MySQLdb
+    from sys import argv
 
-    mycursor = mydb.cursor()
-    stat = """SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"""
-    mycursor.execute(stat, (args[4],))
-    result = mycursor.fetchall()
-    for row in result:
+    usr = str(argv[1])
+    pasw = str(argv[2])
+    db_name = str(argv[3])
+    state_name = str(argv[4])
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=usr, passwd=pasw, db=db_name)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE ( %s );",
+                (state_name, ))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
 
+    cur.close()
+    db.close()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
