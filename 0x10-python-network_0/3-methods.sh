0e439d3d627ac4944fec1308c4e817d939b8eaca
yssrexe <elyasiniyassir40@gmail.com>
2024-05-09 14:13:35 +0100

    task 03

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods
-curl -siLX OPTIONS "$1" | grep allow | cut -d ":" -f 2 | xargs
+curl -siLX OPTIONS "$1" | grep Allow | cut -d ":" -f 2 | xargs
