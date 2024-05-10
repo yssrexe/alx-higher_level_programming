ab069b63d1ba6ef15adc281fa68754f032938d61
yssrexe <elyasiniyassir40@gmail.com>
2024-05-10 12:18:27 +0100

    task 01

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-
+"""
+    a script that takes in a URL, sends a request to the URL and displays
+"""
 import urllib.request
 import sys
 
-arv =  sys.argv
-with urllib.request.urlopen(arv[1]) as rep:
-    data = rep.headers["X-Request-Id"]
-print(data)
+
+def head():
+    arv = sys.argv
+    with urllib.request.urlopen(arv[1]) as rep:
+        data = rep.headers["X-Request-Id"]
+    print(data)
+
+
+if __name__ == "__main__":
+    head()
