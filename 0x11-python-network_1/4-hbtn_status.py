221cac8676d665573add637ec230e4c6a4b7f02a
yssrexe <elyasiniyassir40@gmail.com>
2024-05-10 17:01:59 +0100

    task 04

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,8 @@ Python script that fetches
 
 import requests
 
-data = requests.get("https://alx-intranet.hbtn.io/status")
+data = requests.get('https://alx-intranet.hbtn.io/status')
 
 print("Body response:")
 print(f"\t- type: {type(data.text)}")
-print(f"\t- content: {data}")
+print(f"\t- content: {data.text}")
