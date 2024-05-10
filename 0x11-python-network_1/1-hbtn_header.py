560d477ee7ef148a7c01f979152931e34474bcef
yssrexe <elyasiniyassir40@gmail.com>
2024-05-10 12:15:01 +0100

    task 01

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,7 @@
 import urllib.request
 import sys
 
-arv =  sys.argv[1]
-with urllib.request.urlopen(arv) as rep:
+arv =  sys.argv
+with urllib.request.urlopen(arv[1]) as rep:
     data = rep.headers["X-Request-Id"]
 print(data)
