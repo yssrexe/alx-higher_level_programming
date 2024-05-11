3986a49058dcea89e26daa18f1755b19be940e86
yssrexe <elyasiniyassir40@gmail.com>
2024-05-11 17:07:08 +0100

    task 06

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,7 +6,7 @@ Python script that takes in a URL, sends a request to the URL
 import sys
 import requests
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     value = {'email': email}
