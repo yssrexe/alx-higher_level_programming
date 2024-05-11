29d9733e6c2a778a1a80f5c09026023e738c81cb
yssrexe <elyasiniyassir40@gmail.com>
2024-05-11 15:10:21 +0100

    task 05

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,9 +6,8 @@ Python script that takes in a URL, sends a request to the URL
 import sys
 import requests
 
-arv = sys.argv
-
-urs = requests.get(arv[1])
-data = urs.headers.get("X-Request-Id")
-
-print(data)
+if __name__ == "__main__":
+    arv = sys.argv
+    urs = requests.get(arv[1])
+    data = urs.headers.get("X-Request-Id")
+    print(data)
